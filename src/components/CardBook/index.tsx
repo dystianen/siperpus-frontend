e8c72a60a643b4f788f1080b4e855fef57b296d1
Dystian En <dystian.en@kelaspintar.id>
2024-05-16 22:27:34 +0700

    updated size image

--- src/components/CardBook/index.tsx
+++ src/components/CardBook/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { embedImage } from "@/helper/embedImage";
-import { useMediaQueryFromBreakpoints } from "@/hooks/useMediaQueryFromBreakpoints";
 import { bookTypes } from "@/types/books";
 import { Group, Stack, Text, UnstyledButton } from "@mantine/core";
 import Image from "next/image";
@@ -8,28 +7,17 @@ import { useRouter } from "next/navigation";
 
 const CardBook = ({ item }: { item: bookTypes }) => {
   const router = useRouter();
-  const isMobile = useMediaQueryFromBreakpoints();
-
-  const renderProps = isMobile
-    ? {
-        w: 180,
-        h: 268,
-      }
-    : {
-        w: 200,
-        h: 275,
-      };
 
   return (
     <UnstyledButton onClick={() => router.push(`/books/${item.book_id}`)}>
       <Stack gap={3} align="center" justify="center">
         <Image
           src={embedImage(item.thumbnail)}
-          width={renderProps.w}
-          height={renderProps.h}
+          width={180}
+          height={269}
           alt="book"
           style={{
-            borderRadius: 10,
+            borderRadius: 10
           }}
         />
         <Text ta={"center"} fw={600} lineClamp={1} px={16}>
